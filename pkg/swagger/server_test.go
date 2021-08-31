ce6f51fa95435f665290d9985d84bda3a7c33d67
Ivan <40871973+iakrevetkho@users.noreply.github.com>
2021-08-31 23:56:41 +0300

    fix serving swagger files (#23)

--- pkg/swagger/server_test.go
+++ pkg/swagger/server_test.go
@@ -3,6 +3,8 @@ package swagger_test
 import (
 	// External
 
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/jinzhu/configor"
@@ -20,4 +22,40 @@ func TestNew(t *testing.T) {
 	s, err := swagger.New(conf)
 	assert.NoError(t, err)
 	assert.NotNil(t, s)
+}
+
+func TestServeMainPage(t *testing.T) {
+	conf := new(config.Config)
+	assert.NoError(t, configor.Load(conf))
+
+	s, err := swagger.New(conf)
+	assert.NoError(t, err)
+	assert.NotNil(t, s)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	s.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestServeJS(t *testing.T) {
+	conf := new(config.Config)
+	assert.NoError(t, configor.Load(conf))
+
+	s, err := swagger.New(conf)
+	assert.NoError(t, err)
+	assert.NotNil(t, s)
+
+	req, err := http.NewRequest("GET", "/swagger-ui-bundle.js", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	s.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
 }
