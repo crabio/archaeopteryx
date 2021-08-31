c63553c2e5ca040df99ab8c0908fbd050dc77fd9
iakrevetkho <vanoschukin@yandex.ru>
2021-08-31 22:52:02 +0300

    Add parsing users swagger files

--- pkg/swagger/handlers.go
+++ pkg/swagger/handlers.go
@@ -17,10 +17,20 @@ func (s *Server) createHomePageHandler() (http.Handler, error) {
 	swaggerFilePaths := []string{
 		"health/v1/health_v1.swagger.json",
 	}
+
+	// Parse and add user's swagger files
+	userSwaggerFilePaths, err := getOpenAPIFilesPaths(s.config.Docs.SwaggerDir)
+	if err != nil {
+		s.log.WithError(err).Error("No user's swagger files found")
+	} else {
+		swaggerFilePaths = append(swaggerFilePaths, userSwaggerFilePaths...)
+	}
+
 	swaggerHomeTmpl, err := template.ParseFS(docs_swagger.SwaggerTmpl, "*")
 	if err != nil {
 		return nil, err
 	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Open API home page
 		if err := swaggerHomeTmpl.Execute(w, map[string]interface{}{"filePaths": swaggerFilePaths}); err != nil {
