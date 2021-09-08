c8ab985b0f80bb70535f1229d5d817dadc5c6ccd
iakrevetkho <vanoschukin@yandex.ru>
2021-09-08 23:09:41 +0300

    Add gin http security

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -3,11 +3,12 @@ package http
 import (
 	// External
 
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"github.com/toorop/gin-logrus"
+	ginlogrus "github.com/toorop/gin-logrus"
 
 	// Internal
 	"github.com/iakrevetkho/archaeopteryx/config"
@@ -17,15 +18,17 @@ import (
 )
 
 type Server struct {
-	c   *config.Config
-	log *logrus.Entry
-	r   *gin.Engine
+	c    *config.Config
+	log  *logrus.Entry
+	r    *gin.Engine
+	addr string
 }
 
 func New(c *config.Config, grpcps *grpc_proxy_server.Server, sws *swagger.Server) *Server {
 	s := new(Server)
 	s.log = helpers.CreateComponentLogger("archeaopteryx-http")
 	s.c = c
+	s.addr = ":" + strconv.FormatUint(s.c.RestApiPort, 10)
 	s.r = gin.New()
 	s.r.Use(ginlogrus.Logger(s.log), gin.Recovery())
 
@@ -40,9 +43,22 @@ func New(c *config.Config, grpcps *grpc_proxy_server.Server, sws *swagger.Server
 }
 
 func (s *Server) Run() {
+	httpServer := http.Server{
+		Addr:      ":" + strconv.FormatUint(s.c.RestApiPort, 10),
+		Handler:   s.r,
+		TLSConfig: s.c.Secutiry.TlsConfig}
+
 	go func() {
-		if err := s.r.Run(":" + strconv.FormatUint(s.c.RestApiPort, 10)); err != nil {
-			s.log.WithError(err).Fatal("Couldn't run http server")
+		if s.c.Secutiry.TlsConfig != nil {
+			s.log.WithField("addr", s.addr).Info("Run TLS secure server")
+			if err := httpServer.ListenAndServeTLS("", ""); err != nil {
+				s.log.WithError(err).Fatal("Couldn't run http server")
+			}
+		} else {
+			s.log.WithField("addr", s.addr).Info("Run insecure server")
+			if err := httpServer.ListenAndServe(); err != nil {
+				s.log.WithError(err).Fatal("Couldn't run http server")
+			}
 		}
 	}()
 }
